6c259247d1ac5b2c69a2ce4ec1ffeedd81aaab4c
Shubham Pathak <46400539+shshubham54@users.noreply.github.com>
2021-05-28 12:17:13 +0530

    update app.py

--- app.py
+++ app.py
@@ -89,52 +89,20 @@ def main():
     st.header("WebRTC demo")
 
     object_detection_page = "Real time object detection (sendrecv)"
-    video_filters_page = (
-        "Real time video transform with simple OpenCV filters (sendrecv)"
-    )
-    audio_filter_page = "Real time audio filter (sendrecv)"
-    delayed_echo_page = "Delayed echo (sendrecv)"
-    streaming_page = (
-        "Consuming media files on server-side and streaming it to browser (recvonly)"
-    )
-    video_sendonly_page = (
-        "WebRTC is sendonly and images are shown via st.image() (sendonly)"
-    )
-    audio_sendonly_page = (
-        "WebRTC is sendonly and audio frames are visualized with matplotlib (sendonly)"
-    )
-    loopback_page = "Simple video and audio loopback (sendrecv)"
-    app_mode = st.sidebar.selectbox(
+        app_mode = st.sidebar.selectbox(
         "Choose the app mode",
         [
             object_detection_page,
-            video_filters_page,
-            audio_filter_page,
-            delayed_echo_page,
-            streaming_page,
-            video_sendonly_page,
-            audio_sendonly_page,
-            loopback_page,
         ],
     )
     st.subheader(app_mode)
 
-    if app_mode == video_filters_page:
-        app_video_filters()
-    elif app_mode == object_detection_page:
+
+    if app_mode == object_detection_page:
         app_object_detection()
-    elif app_mode == audio_filter_page:
-        app_audio_filter()
-    elif app_mode == delayed_echo_page:
-        app_delayed_echo()
-    elif app_mode == streaming_page:
-        app_streaming()
     elif app_mode == video_sendonly_page:
         app_sendonly_video()
-    elif app_mode == audio_sendonly_page:
-        app_sendonly_audio()
-    elif app_mode == loopback_page:
-        app_loopback()
+    
 
     logger.debug("=== Alive threads ===")
     for thread in threading.enumerate():
@@ -150,150 +118,6 @@ def app_loopback():
         client_settings=WEBRTC_CLIENT_SETTINGS,
         video_processor_factory=None,  # NoOp
     )
-
-
-def app_video_filters():
-    """ Video transforms with OpenCV """
-
-    class OpenCVVideoProcessor(VideoProcessorBase):
-        type: Literal["noop", "cartoon", "edges", "rotate"]
-
-        def __init__(self) -> None:
-            self.type = "noop"
-
-        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-            img = frame.to_ndarray(format="bgr24")
-
-            if self.type == "noop":
-                pass
-            elif self.type == "cartoon":
-                # prepare color
-                img_color = cv2.pyrDown(cv2.pyrDown(img))
-                for _ in range(6):
-                    img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-                img_color = cv2.pyrUp(cv2.pyrUp(img_color))
-
-                # prepare edges
-                img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-                img_edges = cv2.adaptiveThreshold(
-                    cv2.medianBlur(img_edges, 7),
-                    255,
-                    cv2.ADAPTIVE_THRESH_MEAN_C,
-                    cv2.THRESH_BINARY,
-                    9,
-                    2,
-                )
-                img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-
-                # combine color and edges
-                img = cv2.bitwise_and(img_color, img_edges)
-            elif self.type == "edges":
-                # perform edge detection
-                img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-            elif self.type == "rotate":
-                # rotate image
-                rows, cols, _ = img.shape
-                M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-                img = cv2.warpAffine(img, M, (cols, rows))
-
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-    webrtc_ctx = webrtc_streamer(
-        key="opencv-filter",
-        mode=WebRtcMode.SENDRECV,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-        video_processor_factory=OpenCVVideoProcessor,
-        async_processing=True,
-    )
-
-    if webrtc_ctx.video_processor:
-        webrtc_ctx.video_processor.type = st.radio(
-            "Select transform type", ("noop", "cartoon", "edges", "rotate")
-        )
-
-    st.markdown(
-        "This demo is based on "
-        "https://github.com/aiortc/aiortc/blob/2362e6d1f0c730a0f8c387bbea76546775ad2fe8/examples/server/server.py#L34. "  # noqa: E501
-        "Many thanks to the project."
-    )
-
-
-def app_audio_filter():
-    DEFAULT_GAIN = 1.0
-
-    class AudioProcessor(AudioProcessorBase):
-        gain = DEFAULT_GAIN
-
-        def recv(self, frame: av.AudioFrame) -> av.AudioFrame:
-            raw_samples = frame.to_ndarray()
-            sound = pydub.AudioSegment(
-                data=raw_samples.tobytes(),
-                sample_width=frame.format.bytes,
-                frame_rate=frame.sample_rate,
-                channels=len(frame.layout.channels),
-            )
-
-            sound = sound.apply_gain(self.gain)
-
-            # Ref: https://github.com/jiaaro/pydub/blob/master/API.markdown#audiosegmentget_array_of_samples  # noqa
-            channel_sounds = sound.split_to_mono()
-            channel_samples = [s.get_array_of_samples() for s in channel_sounds]
-            new_samples: np.ndarray = np.array(channel_samples).T
-            new_samples = new_samples.reshape(raw_samples.shape)
-
-            new_frame = av.AudioFrame.from_ndarray(
-                new_samples, layout=frame.layout.name
-            )
-            new_frame.sample_rate = frame.sample_rate
-            return new_frame
-
-    webrtc_ctx = webrtc_streamer(
-        key="audio-filter",
-        mode=WebRtcMode.SENDRECV,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-        audio_processor_factory=AudioProcessor,
-        async_processing=True,
-    )
-
-    if webrtc_ctx.audio_processor:
-        webrtc_ctx.audio_processor.gain = st.slider(
-            "Gain", -10.0, +20.0, DEFAULT_GAIN, 0.05
-        )
-
-
-def app_delayed_echo():
-    DEFAULT_DELAY = 1.0
-
-    class VideoProcessor(VideoProcessorBase):
-        delay = DEFAULT_DELAY
-
-        async def recv_queued(self, frames: List[av.VideoFrame]) -> List[av.VideoFrame]:
-            logger.debug("Delay:", self.delay)
-            await asyncio.sleep(self.delay)
-            return frames
-
-    class AudioProcessor(AudioProcessorBase):
-        delay = DEFAULT_DELAY
-
-        async def recv_queued(self, frames: List[av.AudioFrame]) -> List[av.AudioFrame]:
-            await asyncio.sleep(self.delay)
-            return frames
-
-    webrtc_ctx = webrtc_streamer(
-        key="delay",
-        mode=WebRtcMode.SENDRECV,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-        video_processor_factory=VideoProcessor,
-        audio_processor_factory=AudioProcessor,
-        async_processing=True,
-    )
-
-    if webrtc_ctx.video_processor and webrtc_ctx.audio_processor:
-        delay = st.slider("Delay", 0.0, 5.0, DEFAULT_DELAY, 0.05)
-        webrtc_ctx.video_processor.delay = delay
-        webrtc_ctx.audio_processor.delay = delay
-
-
 def app_object_detection():
     """Object detection demo with MobileNet SSD.
     This model and code are based on
@@ -438,63 +262,6 @@ def app_object_detection():
     )
 
 
-def app_streaming():
-    """ Media streamings """
-    MEDIAFILES = {
-        "big_buck_bunny_720p_2mb.mp4": {
-            "url": "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4",  # noqa: E501
-            "local_file_path": HERE / "data/big_buck_bunny_720p_2mb.mp4",
-            "type": "video",
-        },
-        "big_buck_bunny_720p_10mb.mp4": {
-            "url": "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4",  # noqa: E501
-            "local_file_path": HERE / "data/big_buck_bunny_720p_10mb.mp4",
-            "type": "video",
-        },
-        "file_example_MP3_700KB.mp3": {
-            "url": "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",  # noqa: E501
-            "local_file_path": HERE / "data/file_example_MP3_700KB.mp3",
-            "type": "audio",
-        },
-        "file_example_MP3_5MG.mp3": {
-            "url": "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3",  # noqa: E501
-            "local_file_path": HERE / "data/file_example_MP3_5MG.mp3",
-            "type": "audio",
-        },
-    }
-    media_file_label = st.radio(
-        "Select a media file to stream", tuple(MEDIAFILES.keys())
-    )
-    media_file_info = MEDIAFILES[media_file_label]
-    download_file(media_file_info["url"], media_file_info["local_file_path"])
-
-    def create_player():
-        return MediaPlayer(str(media_file_info["local_file_path"]))
-
-        # NOTE: To stream the video from webcam, use the code below.
-        # return MediaPlayer(
-        #     "1:none",
-        #     format="avfoundation",
-        #     options={"framerate": "30", "video_size": "1280x720"},
-        # )
-
-    WEBRTC_CLIENT_SETTINGS.update(
-        {
-            "media_stream_constraints": {
-                "video": media_file_info["type"] == "video",
-                "audio": media_file_info["type"] == "audio",
-            }
-        }
-    )
-
-    webrtc_streamer(
-        key=f"media-streaming-{media_file_label}",
-        mode=WebRtcMode.RECVONLY,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-        player_factory=create_player,
-    )
-
-
 def app_sendonly_video():
     """A sample to use WebRTC in sendonly mode to transfer frames
     from the browser to the server and to render frames via `st.image`."""
@@ -516,84 +283,6 @@ def app_sendonly_video():
 
             img_rgb = video_frame.to_ndarray(format="rgb24")
             image_place.image(img_rgb)
-        else:
-            logger.warning("AudioReciver is not set. Abort.")
-            break
-
-
-def app_sendonly_audio():
-    """A sample to use WebRTC in sendonly mode to transfer audio frames
-    from the browser to the server and visualize them with matplotlib
-    and `st.pyplog`."""
-    webrtc_ctx = webrtc_streamer(
-        key="loopback",
-        mode=WebRtcMode.SENDONLY,
-        audio_receiver_size=256,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-    )
-
-    fig_place = st.empty()
-
-    fig, [ax_time, ax_freq] = plt.subplots(
-        2, 1, gridspec_kw={"top": 1.5, "bottom": 0.2}
-    )
-
-    sound_window_len = 5000  # 5s
-    sound_window_buffer = None
-    while True:
-        if webrtc_ctx.audio_receiver:
-            try:
-                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
-            except queue.Empty:
-                logger.warning("Queue is empty. Abort.")
-                break
-
-            sound_chunk = pydub.AudioSegment.empty()
-            for audio_frame in audio_frames:
-                sound = pydub.AudioSegment(
-                    data=audio_frame.to_ndarray().tobytes(),
-                    sample_width=audio_frame.format.bytes,
-                    frame_rate=audio_frame.sample_rate,
-                    channels=len(audio_frame.layout.channels),
-                )
-                sound_chunk += sound
-
-            if len(sound_chunk) > 0:
-                if sound_window_buffer is None:
-                    sound_window_buffer = pydub.AudioSegment.silent(
-                        duration=sound_window_len
-                    )
-
-                sound_window_buffer += sound_chunk
-                if len(sound_window_buffer) > sound_window_len:
-                    sound_window_buffer = sound_window_buffer[-sound_window_len:]
-
-            if sound_window_buffer:
-                # Ref: https://own-search-and-study.xyz/2017/10/27/python%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E9%9F%B3%E5%A3%B0%E3%83%87%E3%83%BC%E3%82%BF%E3%81%8B%E3%82%89%E3%82%B9%E3%83%9A%E3%82%AF%E3%83%88%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E3%82%92%E4%BD%9C/  # noqa
-                sound_window_buffer = sound_window_buffer.set_channels(
-                    1
-                )  # Stereo to mono
-                sample = np.array(sound_window_buffer.get_array_of_samples())
-
-                ax_time.cla()
-                times = (np.arange(-len(sample), 0)) / sound_window_buffer.frame_rate
-                ax_time.plot(times, sample)
-                ax_time.set_xlabel("Time")
-                ax_time.set_ylabel("Magnitude")
-
-                spec = np.fft.fft(sample)
-                freq = np.fft.fftfreq(sample.shape[0], 1.0 / sound_chunk.frame_rate)
-                freq = freq[: int(freq.shape[0] / 2)]
-                spec = spec[: int(spec.shape[0] / 2)]
-                spec[0] = spec[0] / 2
-
-                ax_freq.cla()
-                ax_freq.plot(freq, np.abs(spec))
-                ax_freq.set_xlabel("Frequency")
-                ax_freq.set_yscale("log")
-                ax_freq.set_ylabel("Magnitude")
-
-                fig_place.pyplot(fig)
         else:
             logger.warning("AudioReciver is not set. Abort.")
             break
