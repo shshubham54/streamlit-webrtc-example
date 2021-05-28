01cd197387ee9890ff5fa595c335f819d271f6fc
Shubham Pathak <46400539+shshubham54@users.noreply.github.com>
2021-05-29 02:30:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,26 +156,21 @@ def app_object_detection():
         "train",
         "tvmonitor",
     ]
-    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
-
     download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564)
     download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
-
-    DEFAULT_CONFIDENCE_THRESHOLD = 0.5
 
     class Detection(NamedTuple):
         name: str
         prob: float
 
     class MobileNetSSDVideoProcessor(VideoProcessorBase):
-        confidence_threshold: float
         result_queue: "queue.Queue[List[Detection]]"
 
         def __init__(self) -> None:
             self._net = cv2.dnn.readNetFromCaffe(
                 str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH)
             )
-            self.confidence_threshold = DEFAULT_CONFIDENCE_THRESHOLD
+       
             self.result_queue = queue.Queue()
 
         def _annotate_image(self, image, detections):
@@ -183,32 +178,24 @@ def app_object_detection():
             (h, w) = image.shape[:2]
             result: List[Detection] = []
             for i in np.arange(0, detections.shape[2]):
-                confidence = detections[0, 0, i, 2]
-
-                if confidence > self.confidence_threshold:
-                    # extract the index of the class label from the `detections`,
-                    # then compute the (x, y)-coordinates of the bounding box for
-                    # the object
-                    idx = int(detections[0, 0, i, 1])
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-
-                    name = CLASSES[idx]
-                    result.append(Detection(name=name, prob=float(confidence)))
-
-                    # display the prediction
-                    label = f"{name}: {round(confidence * 100, 2)}%"
-                    cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
-                    y = startY - 15 if startY - 15 > 15 else startY + 15
-                    cv2.putText(
-                        image,
-                        label,
-                        (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5,
-                        COLORS[idx],
-                        2,
-                    )
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                name = CLASSES[idx]
+                result.append(Detection(name=name, prob=float(confidence)))
+                # display the prediction
+                label = f"{name}: {round(confidence * 100, 2)}%"
+                cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(
+                    image,
+                    label,
+                    (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    COLORS[idx],
+                    2,
+                )
             return image, result
 
         def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
@@ -234,11 +221,8 @@ def app_object_detection():
         async_processing=True,
     )
 
-    confidence_threshold = st.slider(
-        "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
+
     )
-    if webrtc_ctx.video_processor:
-        webrtc_ctx.video_processor.confidence_threshold = confidence_threshold
 
     if st.checkbox("Show the detected labels", value=True):
         if webrtc_ctx.state.playing:
@@ -260,11 +244,6 @@ def app_object_detection():
                 else:
                     break
 
-    st.markdown(
-        "This demo uses a model and code from "
-        "https://github.com/robmarkcole/object-detection-app. "
-        "Many thanks to the project."
-    )
 
 
 def app_sendonly_video():
